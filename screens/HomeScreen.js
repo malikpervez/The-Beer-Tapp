675fba962d8e86b81d2dac141448f2443612be8a
dutchkillsg <mip12793@gmail.com>
2017-11-10 14:17:15 -0500

    home page alpha

---
+++ screens/HomeScreen.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Button
+  } from 'react-native';
+  
+
+export default class HomeScreen extends Component{
+    static navigationOptions = {
+        title: 'welcome',
+        header: null
+     
+    }
+    render(){
+        const { navigate } = this.props.navigation;
+        return(
+            <View style={styles.container}>
+                <Text style ={styles.welcome}>
+                    The Beer Tapp
+                </Text>
+                <Button
+                    onPress = { () => navigate('Login')}
+                    title = 'Get Started'
+                    color = 'purple'
+                />
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'grey',
+    },
+    welcome: {
+      fontSize: 40,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+  });
+
