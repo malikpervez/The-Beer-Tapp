c36dd3d6e30a98c098c7f19c3eb3d3e83207978b
dutchkillsg <mip12793@gmail.com>
2017-11-22 16:04:40 -0500

    fixed picker

--- screens/BeerPicker.js
+++ screens/BeerPicker.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import { View, Text, Picker, StyleSheet } from 'react-native'
 
 class BeerPicker extends Component {
-   state = {user: [] }
-   updateUser = function(user){
-      this.setState({ user: [] })
-   }
+  constructor(props){
+      super(props);
+      this.state = {
+          beer: []
+      };
+      this.addBeer = this.addBeer.bind(this);
+  }
+  addBeer(newbeer){ 
+    this.setState({
+        beer: [...this.state.beer, newbeer]
+    });
+}
    render() {
       return (
          <View>
-            <Picker selectedValue = {this.state.user} onValueChange = {this.updateUser}>
-               <Picker.Item label = "Steve" value = "steve" />
-               <Picker.Item label = "Ellen" value = "ellen" />
-               <Picker.Item label = "Maria" value = "maria" />
+            <Picker selectedValue = {this.state.beer} onValueChange = {this.addBeer}>
+               <Picker.Item label = "IPA" value = "ipa" />
+               <Picker.Item label = "Pilsner" value = "pilsner" />
+               <Picker.Item label = "Stout" value = "stout" />
             </Picker>
-            <Text style = {styles.text}>{this.state.user}</Text>
+            <Text style = {styles.text}>{this.state.beer}</Text>
          </View>
       )
    }
