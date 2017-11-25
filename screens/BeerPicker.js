230026bcf7eec0e620ae114497636dc7bb91594a
dutchkillsg <mip12793@gmail.com>
2017-11-25 15:55:06 -0500

    styled homepage and fixed listview/picker bug

--- screens/BeerPicker.js
+++ screens/BeerPicker.js
@@ -1,46 +1,60 @@
 import React, { Component } from 'react';
 import { View, Text, Picker, StyleSheet, ListView } from 'react-native'
 
-class BeerPicker extends Component {
-  constructor(props){
+const ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2});
+
+export default class BeerPicker extends Component {
+    constructor(props){
       super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 !== r2});
-        this.state = {
-            beer = [],
-            dataSource: ds.cloneWithRows(['row1', 'row2']),
-        };
-  }
-  addBeer(itemValue, itemIndex){ 
-    this.setState({
-        beer: [...this.state.beer, itemValue]
-    });
-  }
 
-  renderRow(data) {
-    return (
-      <Text>{`\u2022 ${data}`}</Text>
-    );
-  }
+      this.state = {
+        beer: [],
+        beerDataSource: ds.cloneWithRows([]),
+        dataSource: ds.cloneWithRows(['string1', 'string2', 'string3']), //It seems to be not needed...
+        items: [
+          {label: 'Choose Beer', value: 'none'}, 
+          {label: 'IPA', value: 'ipa'}, 
+          {label: 'Pilsner', value: 'pilsner'}, 
+          {label: 'Stout', value: 'stout'}
+        ],
+        selectedItem: 'none'
+      };
 
-   render() {
+      this.addBeer = this.addBeer.bind(this);
+    }
+
+    addBeer(itemValue, itemIndex){ 
+      let newBeerArray = [...this.state.beer, itemValue];
+      this.setState({
+        beer: newBeerArray,
+        selectedItem: itemValue,
+        beerDataSource: ds.cloneWithRows(newBeerArray),
+      });
+    }
+
+    renderRow(data) {
       return (
-         <View>
-            <Picker selectedValue = {this.state.dataSource} onValueChange = {this.addBeer}>
-               <Picker.item label = 'Choose Beer' value = 'none' />
-               <Picker.Item label = "IPA" value = "ipa" />
-               <Picker.Item label = "Pilsner" value = "pilsner" />
-               <Picker.Item label = "Stout" value = "stout" />
-            </Picker>
-            <ListView
-            dataSource={this.state.beer}
-            renderRow={this.renderRow}
-            />
-            
-         </View>
+        <Text>{`\u2022 ${data}`}</Text>
+      );
+    }
+
+    render() {
+      let items = this.state.items.map((item, index) => {
+        return (<Picker.item label={item.label} value={item.value} key={index}/>);
+      });
+
+      return (
+        <View>
+          <Picker selectedValue={this.state.selectedItem} onValueChange = {this.addBeer}>
+            {items}
+          </Picker>
+          <ListView
+            dataSource={this.state.beerDataSource}
+            renderRow={this.renderRow}/>
+        </View>
       )
-   }
+    }
 }
-export default BeerPicker;
 
 const styles = StyleSheet.create({
    text: {
@@ -48,4 +62,4 @@ const styles = StyleSheet.create({
       alignSelf: 'center',
       color: 'black'
    }
-})
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,9 @@ import {
     StyleSheet,
     Text,
     View,
-    Button
+    Button,
+    Image,
+    ImageBackground,
   } from 'react-native';
   
 
@@ -18,30 +20,38 @@ export default class HomeScreen extends Component{
     render(){
         const { navigate } = this.props.navigation;
         return(
-            <View style={styles.container}>
+            <ImageBackground
+            source={require('../assets/images/beer.jpg')}
+            style={styles.container}
+            >
                 <Text style ={styles.welcome}>
-                    The Beer Tapp
+                The Beer Tapp
                 </Text>
                 <Button
                     onPress = { () => navigate('Main')}
                     title = 'Get Started'
                     color = 'purple'
                 />
-            </View>
-        )
+            </ImageBackground>
+       
+    
+        );
     }
 }
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'grey',
+        flex: 1,
+        width: undefined,
+        height: undefined,
+        backgroundColor:'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     welcome: {
       fontSize: 40,
       textAlign: 'center',
       margin: 10,
+      color: 'white'
     },
     instructions: {
       textAlign: 'center',
